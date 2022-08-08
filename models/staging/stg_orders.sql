4b4f662747328815f678525afa84468619558c37
Charly Rosero <charly.rosero@gmail.com>
2022-08-08 17:04:32 +0000

    developing stage_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+select 
+--from raw orders
+o.orderid,
+o.orderdate,
+o.shipdate, 
+o.shipmode, 
+o.ordersellingprice - o.ordercostprice as orderprofit,
+o.ordercostprice,
+o.ordersellingprice,
+--from raw customer
+c.customername,
+c.segment,
+c.country,
+--from raw product
+p.category,
+p.productname, 
+p.subcategory 
+from {{ ref('raw_orders') }} o 
+left join {{ ref('raw_customers') }} c
+on o.customerid=c.customerid
+left join {{ ref('raw_products') }} p
+on o.productid = p.productid
